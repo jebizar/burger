65a7442722c410d2f66baf3876dd02c968530a16
jebizar <jebizar@gmail.com>
2019-01-28 18:20:24 -0800

    update

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) values ('Cheeseburger', true);
+
+INSERT INTO burgers (burger_name, devoured) values ('Crispy Chicken Sandwich', false);
+
+INSERT INTO burgers (burger_name, devoured) values ('Philly Cheesesteak', false);
+
+select * from burgers;
